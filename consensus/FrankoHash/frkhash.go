e9d79a84ee3fb16cd9a58db155e7b1ce4b32a276
jordan69420 <jordan.a.sweetman@icloud.com>
2021-07-26 19:37:29 -0400

    Update frkhash.go

--- consensus/FrankoHash/frkhash.go
+++ consensus/FrankoHash/frkhash.go
@@ -1,11 +1,8 @@
 import (
   //the keccak module thing...
+  "hash"
 )
 
-keccak = //whatever the other thing that you imported was called lol.
-func (k *keccak) frkHash (input) []byte {
-  return keccak.keccak256(keccak.keccak512(input));
-}
 //changes a human-readable difficulty into an invalid header that can be compared against to see if the header's intager value is small enough
 func calcDiffVal(diff int) int {
   var diff_0 float = 10 ** 64
@@ -14,7 +11,30 @@ func calcDiffVal(diff int) int {
   var diff_3 float = diff_0 - diff_2
   return math.Round(diff_3)
 }
-//verifies that a header matches the inputed context, and can then go on to be in the next block or something
+func makeHasher(h hash.Hash) hasher {
+	// sha3.state supports Read to get the sum, use it to avoid the overhead of Sum.
+	// Read alters the state but we reset the hash before every operation.
+	type readerHash interface {
+		hash.Hash
+		Read([]byte) (int, error)
+	}
+	rh, ok := h.(readerHash)
+	if !ok {
+		panic("can't find Read method on hash")
+	}
+	outputLen := rh.Size()
+	return func(dest []byte, data []byte) {
+		rh.Reset()
+		rh.Write(data)
+		rh.Read(dest[:outputLen])
+	}
+}
+func (k *keccak) frkHash (input) []byte {
+  keccak256 := makeHasher(sha3.NewLegacyKeccak256())
+  keccak512 := makeHasher(sha3.NewLegacyKeccak512())
+  return keccak256(keccak512(input));
+}
+//do something like this
 func (h *frkHash) verifyHeader(header, diff, prevHeader, submitted_nonce) bool {
   if (header == nil) {
     return false
