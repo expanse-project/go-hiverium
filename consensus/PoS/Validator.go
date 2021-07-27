a9a46f279a33a1c8c01f98dc9f33ee8c44300261
jordan69420 <jordan.a.sweetman@icloud.com>
2021-07-27 10:37:38 -0400

    remove excess imports

--- consensus/PoS/Validator.go
+++ consensus/PoS/Validator.go
@@ -1,29 +1,8 @@
 package pos
 import (
-	"bytes"
-	"errors"
-	"io"
-	"math/big"
-	"math/rand"
-	"sync"
-	"time"
 
-	"github.com/expanse-org/go-expanse/accounts"
 	"github.com/expanse-org/go-expanse/common"
-	"github.com/expanse-org/go-expanse/common/hexutil"
-	"github.com/expanse-org/go-expanse/consensus"
-	"github.com/expanse-org/go-expanse/consensus/misc"
-	"github.com/expanse-org/go-expanse/core/state"
-	"github.com/expanse-org/go-expanse/core/types"
-	"github.com/expanse-org/go-expanse/crypto"
-	"github.com/expanse-org/go-expanse/ethdb"
-	"github.com/expanse-org/go-expanse/log"
-	"github.com/expanse-org/go-expanse/params"
-	"github.com/expanse-org/go-expanse/rlp"
-	"github.com/expanse-org/go-expanse/rpc"
-	"github.com/expanse-org/go-expanse/trie"
-	lru "github.com/hashicorp/golang-lru"
-	"golang.org/x/crypto/sha3"
+
 )
 
 type validator struct {
