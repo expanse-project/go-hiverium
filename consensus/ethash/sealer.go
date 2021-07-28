1920f99a0d4ef3112c6293aa299097f360d8ba6f
jordan69420 <jordan.a.sweetman@icloud.com>
2021-07-27 21:41:04 -0400

    Update sealer.go

--- consensus/ethash/sealer.go
+++ consensus/ethash/sealer.go
@@ -162,7 +162,7 @@ search:
 				attempts = 0
 			}
 			// Compute the PoW value of this nonce
-			digest, result := hashimotoFull(dataset.dataset, hash, nonce)
+			digest, result := hashimotoFull(hash, nonce)
 			if new(big.Int).SetBytes(result).Cmp(target) <= 0 {
 				// Correct nonce found, create a new header with it
 				header = types.CopyHeader(header)
